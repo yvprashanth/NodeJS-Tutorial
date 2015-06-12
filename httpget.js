ae702394e91b3cecad740841ac4a28a6f0693eb1
Prashanth <tomu644@gmail.com>
2015-06-11 19:09:29 -0700

    httpcollect

--- httpget.js
+++ httpget.js
@@ -2,6 +2,7 @@ var fs = require('fs');
 var path = require('path');
 var http = require('http');
 var str = '';
+
 http.get(process.argv[2], function(res) {
     res.setEncoding('utf8');
     res.on('data', function (chunk) {
